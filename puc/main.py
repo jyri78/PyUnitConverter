5073984a72ede14f438d0644b3d0fab7cc9b8183
Jüri Kormik <jyri78@users.noreply.github.com>
2017-06-12 15:47:24 +0300

    Added functions get_langs() and change_lang()

--- puc/main.py
+++ puc/main.py
@@ -10,23 +10,27 @@ class Main(puc.Frame):
     def __init__(self, parent: puc.Frame, controller: puc.Tk):
         puc.Frame.__init__(self, parent)
 
-        lblTitle = puc.Label(self, text = puc.messages["program.title"],
-                             style="LMT.TLabel")
+        self.lblTitle = puc.Label(self, text = puc.messages["program.title"],
+                                  style="LMT.TLabel")
+
+        self.cmbLang = puc.Combobox(self, state="readonly", width=2)
+        self.cmbLang.bind("<<ComboboxSelected>>", controller.change_lang)
+        self.get_langs()
 
         self.btnConvert = puc.Button(
             self,
             text = puc.messages["main.button.convert"],
             command = lambda: controller.show_frame(puc.Converter))
 
-        btnUnits = puc.Button(self, text = puc.messages["main.button.units"],
-                              command = lambda: controller.show_frame(puc.Units))
+        self.btnUnits = puc.Button(self, text = puc.messages["main.button.units"],
+                                   command = lambda: controller.show_frame(puc.Units))
 
         self.btnSave = puc.Button(self, text = puc.messages["main.button.save"],
                                   command = controller.save_data)
 
         self.btnSave.state(["disabled"])
 
-        btnAbout = puc.Button(
+        self.btnAbout = puc.Button(
             self,
             text = puc.messages["main.button.about"],
             command = lambda: puc.msgBox.showinfo(
@@ -37,16 +41,34 @@ class Main(puc.Frame):
                         + "\n" + "{:>13}".format(puc.messages["program.author"] +":  ")
                         + puc.__author__))
 
-        btnClose = puc.Button(self, text = puc.messages["button.close"],
-                              command = controller.close)
+        self.btnClose = puc.Button(self, text = puc.messages["button.close"],
+                                   command = controller.close)
 
         # If no datafaile found, then disable converting button (no data!)
         self.btnConvert.state(["!disabled" if controller.df_exists else "disabled"])
 
         # Finally show labels and buttons (adds to the grid)
-        lblTitle.grid(row=0, column=0, columnspan=3, padx=10, pady=15, sticky="nw")
+        self.lblTitle.grid(row=0, column=0, columnspan=3, padx=10, pady=15, sticky="nw")
+        self.cmbLang.grid(row=0, column=3, padx=20, pady=15, sticky="ne")
         self.btnConvert.grid(row=1, column=0, padx=10, pady=5)
-        btnUnits.grid(row=1, column=1, padx=10, pady=5)
+        self.btnUnits.grid(row=1, column=1, padx=10, pady=5)
         self.btnSave.grid(row=1, column=3, padx=20, pady=5)
-        btnAbout.grid(row=3, column=1, padx=10, pady=35, sticky="se")
-        btnClose.grid(row=3, column=3, padx=20, pady=35, sticky="se")
+        self.btnAbout.grid(row=3, column=1, padx=10, pady=35, sticky="se")
+        self.btnClose.grid(row=3, column=3, padx=20, pady=35, sticky="se")
+
+    def get_langs(self):
+        out = []
+        langs = puc.glob("./lang/*.py")
+        for l in langs:
+            # Accept only two letter languages
+            out += [l[-5:-3]]
+        self.cmbLang["values"] = sorted(out)
+        self.cmbLang.set(puc.settings["lang"])
+
+    def change_lang(self):
+        self.lblTitle.config(text = puc.messages["program.title"])
+        self.btnConvert.config(text = puc.messages["main.button.convert"])
+        self.btnUnits.config(text = puc.messages["main.button.units"])
+        self.btnSave.config(text = puc.messages["main.button.save"])
+        self.btnAbout.config(text = puc.messages["main.button.about"])
+        self.btnClose.config(text = puc.messages["button.close"])
